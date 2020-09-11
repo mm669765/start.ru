de42b66a509f66ab27885d0e1ad7ad01ceec7b63
mm669765 <68296892+mm669765@users.noreply.github.com>
2020-09-11 22:44:30 +0700

    Update form.py

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import cgi
+import html
 
 form = cgi.FieldStorage()
 text1 = form.getfirst("TEXT_1", "не задано")
 text2 = form.getfirst("TEXT_2", "не задано")
+text1 = html.escape(text1)
+text2 = html.escape(text2)
 
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
